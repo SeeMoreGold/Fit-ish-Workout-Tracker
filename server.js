5bd6605dac22d199026ee67ba15ce4f22e48282b
SeeMoreGold <moreau.cj@gmail.com>
2020-12-20 10:34:00 -0500

    Code for server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT,function(){ 
+    console.log(`App listening on Port ${PORT}`);
+});
