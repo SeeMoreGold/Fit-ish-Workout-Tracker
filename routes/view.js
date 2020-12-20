6b1190b6735a9a0c6c2c2eb37a626b8c39347783
SeeMoreGold <moreau.cj@gmail.com>
2020-12-20 11:23:50 -0500

    Code for view.js routes

---
+++ routes/view.js
@@ -0,0 +1,24 @@
+// Dependencies
+const path = require("path");
+
+// Routes
+module.exports = function (app) {
+    // index route loads homepage
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    // exercise route loads exercise.html
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    // stats route loads stats.html
+    app.get("/stats", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/blog.html"));
+    });
+
+
+
+
+};
